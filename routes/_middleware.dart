046f76984f1d68c5e36a2e55a44584249a99093e
Flutter Intern <flutter1.tork@gmail.com>
2024-05-23 12:47:13 +0600

    dependepcy injection example tried

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:dart_frog/dart_frog.dart';
 
 Handler middleware(Handler handler) {
   // TODO: implement middleware
-  return handler.use((requestLogger()));
+  return handler.use(requestLogger()).use(provider<String>((context) => 'Welcome Shafi to the ocean of backend'));
 }
